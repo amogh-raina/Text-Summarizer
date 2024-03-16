2d28048fb756c97c8086e740cfefd516891a23fe
Amogh Raina <amoghraina204@gmail.com>
2024-03-16 03:26:12 +0100

    Requirements added

--- setup.py
+++ setup.py
@@ -6,6 +6,23 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer"
-AUTOR - "amogh-raina"
-SRC_REPO = "text_Summarizer"
+AUTHOR = "amogh-raina"
+SRC_REPO = "TextSummarizer"
 AUTHOR_EMAIL = "amoghraina391@gmail.com"
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description="A package to summarize text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
